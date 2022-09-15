e242305fdda02b1dbfae28452279724b6997de83
thanujakaviti <94030167+thanujakaviti@users.noreply.github.com>
2022-09-15 23:09:56 +0530

    m1() called

--- src/main/java/in/jt/A.java
+++ src/main/java/in/jt/A.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 public class A {
 	public A() {
 		System.err.println(" A :: Constructor");
+		m1();
 	}
 	void m1() {
 		System.out.println(" A :: m1");
